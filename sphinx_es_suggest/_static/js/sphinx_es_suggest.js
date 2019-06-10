0ead607b7c52fd34a8ce3c78becfe3377fa7151c
dojutsu-user <vaibhgupt199@gmail.com>
2019-06-11 00:48:48 +0530

    Update comments

--- sphinx_es_suggest/_static/js/sphinx_es_suggest.js
+++ sphinx_es_suggest/_static/js/sphinx_es_suggest.js
@@ -272,14 +272,11 @@ const fetchAndGenerateResults = (search_url, projectName) => {
  *
  *  <div class="search__outer__wrapper search__backdrop">
  *      <div class="search__outer">
- *          <div class="search__cross">
- *              <img
- *                  class="search__cross_img"
- *                  alt="Close"
- *                  src="https://image.flaticon.com/icons/svg/59/59836.svg' />"
- *              />
- *              <input class="search__outer__input" placeholder="Search ...">
+ *          <div class="search__cross" title="Close">
+ *              <!--?xml version='1.0' encoding='UTF-8'?-->
+ *              <svg class="search__cross__img" width="15px" height="15px" enable-background="new 0 0 612 612" version="1.1" viewBox="0 0 612 612" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><polygon points="612 36.004 576.52 0.603 306 270.61 35.478 0.603 0 36.004 270.52 306.01 0 576 35.478 611.4 306 341.41 576.52 611.4 612 576 341.46 306.01"></polygon></svg>
  *          </div>
+ *          <input class="search__outer__input" placeholder="Search ...">
  *      </div>
  *  </div>
  *
