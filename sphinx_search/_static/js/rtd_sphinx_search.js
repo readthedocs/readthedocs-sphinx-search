9fa5877b4b07f28632122c23108c37655544aa10
dojutsu-user <vaibhgupt199@gmail.com>
2019-06-14 10:02:10 +0530

    spacing in js file

--- sphinx_search/_static/js/rtd_sphinx_search.js
+++ sphinx_search/_static/js/rtd_sphinx_search.js
@@ -1,4 +1,4 @@
 window.addEventListener("DOMContentLoaded", event => {
-  console.log("DOM fully loaded and parsed");
-  console.log(READTHEDOCS_DATA);
+    console.log("DOM fully loaded and parsed");
+    console.log(READTHEDOCS_DATA);
 });
