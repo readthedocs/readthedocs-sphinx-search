0297af8b0a754de9641fbc7f2ec0eb22b7fa5f62
dojutsu-user <vaibhgupt199@gmail.com>
2019-07-19 22:43:06 +0530

    undo headless

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,7 @@ pytest_plugins = 'sphinx.testing.fixtures'
 
 @pytest.fixture
 def firefox_options(firefox_options):
-#     firefox_options.add_argument('-headless')
+    firefox_options.add_argument('-headless')
     return firefox_options
 
 
