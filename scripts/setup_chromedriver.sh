62a10be1982d6d18551b03a63c2650032e5aed97
dojutsu-user <vaibhgupt199@gmail.com>
2019-10-29 20:10:40 +0530

    dont update chromedriver in this PR

--- scripts/setup_chromedriver.sh
+++ scripts/setup_chromedriver.sh
@@ -1,4 +1,4 @@
-wget -N https://chromedriver.storage.googleapis.com/77.0.3865.40/chromedriver_linux64.zip -P ~/
+wget -N https://chromedriver.storage.googleapis.com/75.0.3770.8/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
 sudo mv -f ~/chromedriver /usr/local/bin/
