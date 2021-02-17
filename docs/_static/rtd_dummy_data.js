771f14f8a6e6fe500b162f366e1bc6f7d049fb6e
Santos Gallegos <stsewd@protonmail.com>
2021-02-17 10:47:16 -0500

    Make it easy to test locally
    Additionally, disabling the CORS check should be done.
    I'm using https://addons.mozilla.org/en-US/firefox/addon/cors-everywhere/ for that.

--- docs/_static/rtd_dummy_data.js
+++ docs/_static/rtd_dummy_data.js
@@ -2,5 +2,5 @@ var READTHEDOCS_DATA = {
   'project': 'readthedocs-sphinx-search',
   'version': 'latest',
   'language': 'en',
-  'proxied_api_host': 'https://readthedocs-sphinx-search.readthedocs.io/_',
+  'proxied_api_host': 'https://readthedocs.org',
 };
